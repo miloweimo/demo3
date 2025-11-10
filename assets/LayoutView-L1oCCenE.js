d1ee43e470a877168aef98a232133b2b8c1d49af
Wei Mo <miloweimo@outlook.com>
2025-11-10 21:30:23 +0800

    打包脚本

---
+++ assets/LayoutView-L1oCCenE.js
@@ -0,0 +1,1 @@
+import{_ as o,r,c as t,o as c}from"./index-DJFfFn0k.js";const n={};function s(_,a){const e=r("router-view");return c(),t(e)}const i=o(n,[["render",s]]);export{i as default};
---
+++ assets/index-CDKRJ86j.js
@@ -0,0 +1,1 @@
+import{_ as n,f as o,g as r,o as t}from"./index-DJFfFn0k.js";const s={};function a(c,e){return t(),o("div",null,[...e[0]||(e[0]=[r("h1",null,"Demo2",-1)])])}const f=n(s,[["render",a]]);export{f as default};
